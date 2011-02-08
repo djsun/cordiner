2c6136c1ca342c6960e1055d077ddd17445e1eb9
David James <djames@sunlightfoundation.com>
2011-02-08 10:41:17 -0500

    rails new cordiner -O -T -J

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cordiner::Application.config.secret_token = '255a7cd1d30ff666212b1c1b2f7e5fcd0fa2e208da185bbdd672b50d5d1a0f35f79a8c9625c3f491868b22b3cee0e34d0afbe1b154f88752114c8a6238783ba4'
